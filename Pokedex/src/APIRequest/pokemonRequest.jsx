53eaf2d9a553c0b253eff5b74eb4bcddcebbb1bc
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2023-03-19 10:36:01 -0500

    just pulled data and displayed it

--- Pokedex/src/APIRequest/pokemonRequest.jsx
+++ Pokedex/src/APIRequest/pokemonRequest.jsx
@@ -1,25 +1,41 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export const PokemonAPI = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState();
 
   useEffect(() => {
-    fetchData();
+        fetchData();
   }, []);
 
-  async function fetchData() {
-    const response = await fetch('https://pokeapi.co/api/v2/ability/1/');
-    const json = await response.json();
-    setData(json.results);
+  async function fetchData() {  // defines an async function named "fetchData"
+    try {  // starts a try-catch block for error handling
+      const pokemonPromises = [];  // creates an empty array named "pokemonPromises"
+      for (let i = 1; i <= 15; i++) {  // loops through the numbers 1 to 15
+        pokemonPromises.push(axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`));  // sends an HTTP GET request to the PokeAPI for each number and pushes the resulting promise to "pokemonPromises"
+      }
+      const pokemonResponses = await Promise.all(pokemonPromises);  // waits for all the promises in "pokemonPromises" to resolve and returns an array of their results, which is assigned to "pokemonResponses"
+      const pokemonData = pokemonResponses.map(response => response.data);  // creates an array of Pokemon data objects by mapping over "pokemonResponses" and extracting the "data" property from each response
+      console.log(pokemonData);
+      setData(pokemonData);  // updates the component's state with the array of Pokemon data objects
+    } catch (error) {  // handles any errors thrown within the try block
+      console.error(error);  // logs the error to the console
+      setData(null);  // sets the component's state to null to indicate an error
+    }
   }
-  console.log(data);
+  
+
+
 
   return (
     <div>
       {data ? (
-        <ul>
+        <ul className='text-center'>
           {data.map((pokemon) => (
-            <li key={pokemon.name}>{pokemon.name}</li>
+            <li className='mx-auto' key={pokemon.id}>
+                {pokemon.name}
+                <img className='mx-auto' src={pokemon.sprites.front_default} alt="" />
+            </li>
           ))}
         </ul>
       ) : (
