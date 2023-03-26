b24c9a96fdfcdd83299506e9d38867455d978246
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2023-03-26 10:29:36 -0500

    changed useParams

--- Pokedex/src/main.jsx
+++ Pokedex/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import {createBrowserRouter, RouterProvider,useParams} from 'react-router-dom';
+import {createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ErrorPage from './error-page';
 import { IndividualPokemon } from './pages/IndividualPokemon';
 import PokemonPage from './pages/PokemonPage';
