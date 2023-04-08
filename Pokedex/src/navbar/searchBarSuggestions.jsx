1d7230a8c0b7d5b5067a7eae100c2841efee6e86
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2023-04-08 10:46:35 -0500

    tried to add debounce and also added comments to why we are using usecallback and usememo

--- Pokedex/src/navbar/searchBarSuggestions.jsx
+++ Pokedex/src/navbar/searchBarSuggestions.jsx
@@ -4,10 +4,6 @@ import { usePokemonAPI } from "../APIRequest/pokemonRequest";
 export const SearchBarSuggestions = ({ value, setValue }) => {
   const { data } = usePokemonAPI(150);
 
-  useEffect(() => {
-    console.log("value has changed");
-  }, [data]);
-
   const filteredData = useMemo(() => {
     if (data) {
       const searchTerm = value.toLowerCase();
@@ -19,6 +15,10 @@ export const SearchBarSuggestions = ({ value, setValue }) => {
     }
     return [];
   }, [data, value]);
+  //note while the useMemo here might not be the most optimal for smaller sets of data
+  //this would be an example and if we had a large data set this would be optimal for preformance since
+  //it would memoize the results and not have to recalculate everytime unless data or value change.
+  //also useMemo returns a memoized value that is expensive to compute
 
   const handleSearch = useCallback(
     (searchTerm) => {
@@ -26,7 +26,8 @@ export const SearchBarSuggestions = ({ value, setValue }) => {
     },
     [setValue]
   );
-
+  //useCallback returns a memoized function.
+  //should be used when functions are passed down to a child compoenent or if the function runs many times.
   return (
     <div
       className={`absolute bg-gray-900 top-full left-0 right-0 shadow-lg rounded-md overflow-hidden mt-1 ${
@@ -49,3 +50,4 @@ export const SearchBarSuggestions = ({ value, setValue }) => {
 };
 
 export default SearchBarSuggestions;
+//questioning about useMemo and useCallback in the search function
